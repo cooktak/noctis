47cc7c9340a0ebd674e304a952ad44fa1dc3623f
Joosung Park <admin@slowmotion.dev>
2020-05-27 13:03:57 +0900

    feat: Basic web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, get, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+        .service(index)
+    })
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
 }
